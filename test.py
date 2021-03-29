412305995bb8f6db803b29d9807a39798ff6722f
mohammadmahdi <76968302+mrmahdi8800@users.noreply.github.com>
2021-03-29 11:15:17 +0430

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from threading import Timer
+x=datetime.todey()
+y=x.replace(dey=x.dey+1, hour=11 , minute=5, second=0, microsecond=0)
+delta_t=y_x
+secs=delta_t.second+1
+def hello_world():
+   print("Hello, my name is Mohammad Mehdi")
+   #...
+t = Timer(secs, hello_world)
+t.start()
